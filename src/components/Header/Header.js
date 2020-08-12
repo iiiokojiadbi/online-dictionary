2972e87f2122320d1daad9ac63e577a330624192
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-12 17:48:32 +0300

    feat: реализовать переход по ссылке

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={classes.header}>
-      <button type="button">Word Keeper</button>
-      <button type="button">&#9733; Starred Words</button>
+      <NavLink
+        activeClassName={classes.active}
+        className={classes.link}
+        exact
+        to="/"
+      >
+        Word Keeper
+      </NavLink>
+      <NavLink
+        activeClassName={classes.active}
+        className={classes.link}
+        to="/starred"
+      >
+        &#9733; Starred Words
+      </NavLink>
     </header>
   );
 };
