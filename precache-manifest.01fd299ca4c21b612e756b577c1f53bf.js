8b4c08b2f8547137404b5740ef5b2593e90669c5
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-14 02:01:45 +0300

    Updates

---
+++ precache-manifest.01fd299ca4c21b612e756b577c1f53bf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb91e1521063651bc895360cd39a8999",
+    "url": "/iiiokojiadbi/online-dictionary/index.html"
+  },
+  {
+    "revision": "93d32ba11564a4180039",
+    "url": "/iiiokojiadbi/online-dictionary/static/css/main.c530cb17.chunk.css"
+  },
+  {
+    "revision": "991e71d077c5ef52bd90",
+    "url": "/iiiokojiadbi/online-dictionary/static/js/2.6a50feab.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/iiiokojiadbi/online-dictionary/static/js/2.6a50feab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d32ba11564a4180039",
+    "url": "/iiiokojiadbi/online-dictionary/static/js/main.185b127f.chunk.js"
+  },
+  {
+    "revision": "2a5fc78461e5a81e589e",
+    "url": "/iiiokojiadbi/online-dictionary/static/js/runtime-main.445e2d15.js"
+  },
+  {
+    "revision": "333ee0ee5989e593812c23ca2dd7bc24",
+    "url": "/iiiokojiadbi/online-dictionary/static/media/PTSans-Bold.333ee0ee.ttf"
+  },
+  {
+    "revision": "4ea26cd5e7f64894d6c2451446f7dda5",
+    "url": "/iiiokojiadbi/online-dictionary/static/media/PTSans-Regular.4ea26cd5.ttf"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/iiiokojiadbi/online-dictionary/static/media/close.5df32a06.svg"
+  },
+  {
+    "revision": "95eebfdbf99d168a8c6cc9cfe456667b",
+    "url": "/iiiokojiadbi/online-dictionary/static/media/headline.95eebfdb.svg"
+  },
+  {
+    "revision": "1b5c8a2b7d5240608758120b46c1a8f3",
+    "url": "/iiiokojiadbi/online-dictionary/static/media/star_black.1b5c8a2b.svg"
+  },
+  {
+    "revision": "981d4779bd9b3731741d90491b8dfa06",
+    "url": "/iiiokojiadbi/online-dictionary/static/media/star_border.981d4779.svg"
+  },
+  {
+    "revision": "ca9a8dda48e6a2aeda544d2454f64eb2",
+    "url": "/iiiokojiadbi/online-dictionary/static/media/star_white.ca9a8dda.svg"
+  }
+]);
