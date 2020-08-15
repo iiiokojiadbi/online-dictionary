3791646c97024d5864b30ca92aa31836f0d9c33c
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-08-16 01:26:06 +0300

    feat: добавить задержку после ввода текста в инпут

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,17 +1,37 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import classes from './Input.module.css';
 
 import { SearchValueContext } from './../../context/SearchValueContext';
+import { IsStarredContext } from './../../context/IsStarredContext';
 
 const Input = () => {
   const { handleChangeSearch, inputWord = '' } = useContext(SearchValueContext);
+  const isStarred = useContext(IsStarredContext);
 
   const [inputValue, setInputValue] = useState(inputWord);
+  const [delay, setDelay] = useState(true);
+  const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    if (!delay && !isStarred) {
+      handleChangeSearch({ word: inputValue });
+      setDelay(true);
+    }
+
+    if (isStarred) {
+      handleChangeSearch({ word: inputValue });
+    }
+  }, [inputValue, delay, isStarred]);
 
   const handleChange = (evt) => {
     const { value } = evt.target;
-    handleChangeSearch({ word: value });
     setInputValue(value);
+
+    if (!isStarred) {
+      clearTimeout(timer);
+      const timerDelay = setTimeout(() => setDelay(false), 1500);
+      setTimer(timerDelay);
+    }
   };
 
   return (
